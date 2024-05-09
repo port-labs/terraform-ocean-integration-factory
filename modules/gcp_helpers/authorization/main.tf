caf9014a72a8b882d450457083534f453d24be06
Matan <51418643+matan84@users.noreply.github.com>
2024-05-09 18:23:35 +0300

    Add projects filtering to the GCP integration (#5)

--- modules/gcp_helpers/authorization/main.tf
+++ modules/gcp_helpers/authorization/main.tf
@@ -12,7 +12,20 @@ resource "google_organization_iam_custom_role" "ocean_integration_iam_org_role" 
 }
 
 resource "google_organization_iam_member" "ocean_integration_iam_member" {
-  org_id=var.organization
-  role= google_organization_iam_custom_role.ocean_integration_iam_org_role.name
-  member= "serviceAccount:${google_service_account.ocean_integration_service_account.email}"
+  count  = length(var.projects) == 0 ? 1 : 0
+  org_id = var.organization
+  role   = google_organization_iam_custom_role.ocean_integration_iam_org_role.name
+  member = "serviceAccount:${google_service_account.ocean_integration_service_account.email}"
+}
+
+
+resource "google_project_iam_binding" "custom_role_binding" {
+  count = length(var.projects)
+
+  project = var.projects[count.index]
+  role    = google_organization_iam_custom_role.ocean_integration_iam_org_role.name
+
+  members = [
+    "serviceAccount:${google_service_account.ocean_integration_service_account.email}"
+  ]
 }
