e507e40ea67ffbc1fb8d1a9a1387f87160aa3c5a
MatanGevaPort <matang@getport.io>
2024-07-15 11:22:07 +0300

    Added projects exclustion to gcp

--- modules/gcp_helpers/authorization/main.tf
+++ modules/gcp_helpers/authorization/main.tf
@@ -1,3 +1,15 @@
+data "google_projects" "all" {
+  filter = "parent.id=${var.organization}"
+}
+
+locals {
+  has_specific_projects = length(var.projects) > 0
+  has_excluded_projects = length(var.excluded_projects) > 0
+  filtered_projects     = local.has_excluded_projects ? [for project in data.google_projects.all.projects : project.project_id if !contains(var.excluded_projects, project.project_id)] : []
+
+  included_projects = local.has_specific_projects ? var.projects : (local.has_excluded_projects ? local.filtered_projects : [])
+}
+
 resource "google_service_account" "ocean_integration_service_account" {
   project      = var.project
   account_id   = var.service_account_id
@@ -12,7 +24,7 @@ resource "google_organization_iam_custom_role" "ocean_integration_iam_org_role" 
 }
 
 resource "google_organization_iam_member" "ocean_integration_iam_member" {
-  count  = length(var.projects) == 0 ? 1 : 0
+  count  = length(local.included_projects) == 0 ? 1 : 0
   org_id = var.organization
   role   = google_organization_iam_custom_role.ocean_integration_iam_org_role.name
   member = "serviceAccount:${google_service_account.ocean_integration_service_account.email}"
@@ -20,9 +32,9 @@ resource "google_organization_iam_member" "ocean_integration_iam_member" {
 
 
 resource "google_project_iam_binding" "custom_role_binding" {
-  count = length(var.projects)
+  count = length(local.included_projects)
 
-  project = var.projects[count.index]
+  project = local.included_projects[count.index]
   role    = google_organization_iam_custom_role.ocean_integration_iam_org_role.name
 
   members = [
