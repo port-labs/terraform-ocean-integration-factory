699ef29373659bab7a57265e68d08ef0c3d64cbe
Tom Tankilevitch <59158507+Tankilevitch@users.noreply.github.com>
2024-07-29 09:28:03 +0300

    [GCP] Add `gcp_ocean_integration_location` to installation (#19)

--- examples/gcp_integration_with_real_time/variables.tf
+++ examples/gcp_integration_with_real_time/variables.tf
@@ -55,6 +55,12 @@ variable "gcp_ocean_integration_image" {
   type        = string
   description = "The Artifact Registry / Dockerhub image to deploy. Run docker pull --platform=linux/amd64 ghcr.io/port-labs/port-ocean-gcp and then push it to your preffered registry. For example: europe-west2-docker.pkg.dev/<project_id>/<your-artifact-registry>/<your_image_id>@sha256:123456789mysha987654321 OR europe-west2-docker.pkg.dev/<project_id>/<your-artifact-registry>/<your_image_id>"
 }
+
+variable "gcp_ocean_integration_cloud_run_location" {
+  type        = string
+  default     = "europe-west1"
+}
+
 variable "environment_variables" {
   type    = list(map(string))
   default = null
